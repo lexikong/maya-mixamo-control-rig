4e3fe9dd549a23e3612b2f19c22974f564f4420f
Lexi <shinings979@gmail.com>
2025-11-16 18:02:47 -0800

    Ankle Ik very messy version

--- src/leg.py
+++ src/leg.py
@@ -2,6 +2,7 @@ from limb import MixamoLimb
 from maya import cmds
 from utils import createCircleCtrl
 from constants import YELLOW
+from shapes import drawCtrlCube
 
 
 class MixamoLeg(MixamoLimb):
@@ -49,7 +50,7 @@ class MixamoLeg(MixamoLimb):
         # create ankle FK control
         # using a joint as the controller
         # so that the rotation is around world Y and local X,Z
-        ctrlJnt = cmds.joint(name=f"{self.ctrlNameSpace}:ctrl{jntName}")
+        ctrlJnt = cmds.joint(name=f"{self.ctrlNameSpace}:ctrl{jntName}",p=[0,0,0])
         ctrlCircle = cmds.circle(radius=10.0)
         cmds.makeIdentity(ctrlCircle, apply=True, r=True)
         circleShape = cmds.listRelatives(ctrlCircle, shapes=True)[0]
@@ -84,5 +85,50 @@ class MixamoLeg(MixamoLimb):
 
         cmds.orientConstraint(ctrlJnt, jntNameFull, mo=True)
 
+    def createIkHandle(self):
+        ctrlJnt = cmds.joint(name=f"{self.ctrlNameSpace}:ctrl{self.thirdJnt}Ik", p=[0,0,0])
+        cubeCtrl = drawCtrlCube(name=f"{self.ctrlNameSpace}:cube{self.thirdJnt}", size=5.0)
+        cmds.makeIdentity(cubeCtrl, apply=True, r=True)
+        cubeShape = cmds.listRelatives(cubeCtrl, shapes=True)[0]
+
+        # attach circle shape to joint
+        cmds.parent(cubeShape, ctrlJnt, add=True, shape=True)
+        # set the joint display to none
+        cmds.setAttr(f"{ctrlJnt}.drawStyle", 2)
+        cmds.delete(cubeCtrl)
+
+        loc = cmds.spaceLocator(p=[0,0,0])[0]
+        grp = cmds.group([ctrlJnt, loc], name=f"{self.ctrlNameSpace}:zero{self.thirdJnt}Ik")
+        jntNameFull = f"{self.jntNameSpace}:{self.thirdJnt}"
+        cmds.matchTransform(grp, jntNameFull)
+
+        pos = cmds.xform(loc, q=True, ws=True, t=True)
+        newPos = (pos[0], pos[1] + 5, pos[2])
+        cmds.xform(loc, ws=True, t=newPos)
+
+        # set aim constraint so that the ctrl joint aligns with world Y
+        toeJntFull = f"{self.jntNameSpace}:{self.toeJnt}"
+        aimConst = cmds.aimConstraint(loc, ctrlJnt, aimVector=[0,0,1], upVector=[0,1,0], wut="object", wuo=toeJntFull)
+        cmds.delete(aimConst)
+        cmds.delete(loc)
+        # freeze transformation
+        cmds.makeIdentity(ctrlJnt, apply=True, r=True)
+
+        self.ikCtrl = ctrlJnt
+
+        ikHandle = cmds.ikHandle(name=f"{self.ctrlNameSpace}:ikHandle{self.thirdJnt}",
+                                 startJoint=self._firstJntIkFull,
+                                 endEffector=self._thirdJntIkFull,
+                                 solver="ikRPsolver")[0]
+        cmds.parent(ikHandle, ctrlJnt)
+        cmds.hide(ikHandle)
+
+        # set orient constraint from ikCtrl to wrist joint
+        # TODO: only works for arms
+        #cmds.orientConstraint(ikCtrl, self._thirdJntIkFull)
+        return ikHandle
+    
     def endJointOrient(self):
-        pass
+        cmds.orientConstraint(self.ikCtrl, self._thirdJntIkFull, mo=True)
+
+
