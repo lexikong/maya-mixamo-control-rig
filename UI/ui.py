99d604c2d04d1a8cac84919f23f7e9818348cb2c
Lexi <shinings979@gmail.com>
2025-12-16 21:16:18 -0800

    Add dev mode

--- UI/ui.py
+++ UI/ui.py
@@ -9,8 +9,44 @@ SKELETON_MENU_ID = 'skeletonLodMenu'
 TEXT_HEIGHT = 30
 TEXT_FIELD_WIDTH = (120, 100)
 
+DEV_MODE = True
+
 
 def createUI():
+    # DEV ONLY
+    if (DEV_MODE):
+        import sys
+        import importlib
+
+        modules_to_reload = [
+            "mixamoControlRig.Controls.startup",
+            "mixamoControlRig.Controls.Rigs",
+            "mixamoControlRig.Controls.Rigs.arm",
+            "mixamoControlRig.Controls.Rigs.leg",
+            "mixamoControlRig.Controls.Rigs.limb",
+            "mixamoControlRig.Controls.Rigs.cog",
+            "mixamoControlRig.Controls.Rigs.fingers",
+            "mixamoControlRig.Controls.Rigs.foot",
+            "mixamoControlRig.Controls.Rigs.head",
+            "mixamoControlRig.Controls.Rigs.shoulders",
+            "mixamoControlRig.Controls.Rigs.spine",
+            "mixamoControlRig.Controls.Rigs.world",
+            "mixamoControlRig.Controls.Utils",
+            "mixamoControlRig.Controls.Utils.constants",
+            "mixamoControlRig.Controls.Utils.helpers",
+            "mixamoControlRig.Controls.Utils.limbConfig",
+            "mixamoControlRig.Controls.Utils.limbParams",
+            "mixamoControlRig.Controls.Utils.orientJoints",
+            "mixamoControlRig.Controls.Utils.shapes",
+            "mixamoControlRig.Controls.Utils.userInput",
+            "mixamoControlRig.UI",
+            "mixamoControlRig.UI.ui",
+        ]
+
+        for m in modules_to_reload:
+            if m in sys.modules:
+                importlib.reload(sys.modules[m])
+
     # building UI
     windowName = 'MixamoControlRigWindow'
 
